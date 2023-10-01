759fc49dc83ca127d075884554af6cea0763902a
Bruno Costa <80495655+BrunoCosta-P@users.noreply.github.com>
2023-10-01 00:07:45 -0300

    feat: instalando dependencias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@invictus.codes/nuxt-vuetify'
+    'vuetify-nuxt-module'
   ],
-
-  // vuetify: {
-  //   vuetifyOptions: {
-  //   },
-  //   moduleOptions: {
-
-  //     treeshaking: false,
-  //     useIconCDN: true,
-
-  //     styles: 'sass',
-  //     autoImport: true,
-  //     useVuetifyLabs: true,
-  //   }
-  // },
 
   devtools: { enabled: true }
 })
